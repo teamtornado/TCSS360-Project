ca08c4fd9b5d521f70ec1600c9f6086d7d66b306
Ewerk4 <Eric@hooverzone.com>
2019-05-30 15:26:08 -0700

    Model work
    - Started to implement schema database
    - Started to implement the parser (basically empty)
    - fleshed out the data types somewhat.

--- src/model/ItemElement.java
+++ src/model/ItemElement.java
@@ -1,7 +1,26 @@
 package model;
 
 public class ItemElement {
-	private String itemName;
-	private String itemDescription;
-	private String itemValue;
+	private String myItemName;
+	private String myItemDescription;
+	private String myItemValue;
+
+	public ItemElement(final String theName, final String theItemDescription,
+			final String theItemValue) {
+		this.myItemName = theName;
+		this.myItemDescription = theItemDescription;
+		this.myItemValue = theItemValue;
+	}
+
+	public String getItemName() {
+		return myItemName;
+	}
+
+	public String getItemDescription() {
+		return myItemDescription;
+	}
+
+	public String getItemValue() {
+		return myItemValue;
+	}
 }
---
+++ src/model/SchemaTypes.java
@@ -0,0 +1,29 @@
+package model;
+
+public class SchemaTypes {
+
+	// Macro types
+	public static final String APPLICANCE = "APPLIANCE";
+	public static final String HEATING = "HEATING";
+	public static final String OUTDOOR = "OUTDOOR";
+	public static final String LIGHTING = "LIGHTING";
+
+	// Types
+	public static final String STOVE = "STOVE";
+	public static final String LIGHT = "LIGHT";
+	public static final String FURNACE = "FURNACE";
+	public static final String REFRIGERATOR = "REFRIGERATOR";
+
+	// ----
+	
+	// Field names
+	public static final String BTU = "BTU";
+	public static final String BRAND = "BRAND";
+	public static final String LUMINS = "LUMINS";
+	public static final String VOLTAGE = "VOLTAGE";
+	public static final String AVERAGE_USE = "AVERAGE_USE";
+	public static final String DAILY_USE = "DAILY_USE";
+	public static final String ENERGY_USE = "ENERGY_USE";
+	public static final String VOLUME_CAPACITY = "VOLUME_CAPACITY";
+
+}
