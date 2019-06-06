2219bb81db2e6b07aea311013c43513b3db5b4a8
aiinori <pcongminh1@gmail.com>
2019-06-06 04:52:56 -0700

    Clean up some code

--- src/gui/createpanels/ItemInputPanel.java
+++ src/gui/createpanels/ItemInputPanel.java
@@ -6,35 +6,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JList;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
 
 import controller.SchemaController;
-import model.SchemaField;
-import model.schemautil.SchemaTypes;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
 
-public class ItemInputPanel extends JPanel {
+public class ItemInputPanel extends JPanel implements ActionListener {
 
 	/**
 	 * Default serial version
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * 
+	 */
 	private SchemaController myRules;
 	
-	private JComboBox<String> myChildTypeDropDown;
-	
+	/**
+	 * 
+	 */
 	private JComboBox<String> myItemtypeDropDown;
 
 	/**
@@ -66,58 +64,14 @@ public class ItemInputPanel extends JPanel {
 		String[] parentString = parentList.stream().toArray(String[] :: new);
 		myItemtypeDropDown = new JComboBox<String>(parentString);
 		itemTypeChooserPanel.add(myItemtypeDropDown);
-		ActionListener itemChangeListener = new ActionListener() {
-			
-			@SuppressWarnings("unchecked")
-			@Override
-			public void actionPerformed(ActionEvent e) {		
-			
-//				System.out.println("?");
-				JComboBox<String> cb = (JComboBox<String>) e.getSource();
-				String parentName = (String) cb.getSelectedItem();
-				System.out.println(parentName);
-				// Get all the child type
-				List<String> childTypes = myRules.getChildTypes(parentName);
-				if (childTypes.size() != 0) {
-					// Clear out the old type
-					myItemtypeDropDown.removeActionListener(itemChangeListener);
-					myItemtypeDropDown.removeAllItems();
-					for (String child : childTypes) {
-						myItemtypeDropDown.addItem(child);
-					}
-					myItemtypeDropDown.addActionListener(itemChangeListener); 
-				} else {
-					JOptionPane.showMessageDialog(currentItemsViewer, "There are no more child type");
-				}
-				
-			}
-		};
-		myItemtypeDropDown.addActionListener(itemChangeListener);
-		
-//		String[] childStrings = {"<Select a parent type>"};
-//		myChildTypeDropDown = new JComboBox<String>(childStrings);
-//		myChildTypeDropDown.setEnabled(false);
-//		myChildTypeDropDown.addActionListener(this);
-//		itemTypeChooserPanel.add(myChildTypeDropDown);
+		myItemtypeDropDown.addActionListener(this);
 
 		JButton addItemButton = new JButton("Add Item");
 		itemTypeChooserPanel.add(addItemButton);
 		
 		JButton resetButton = new JButton("Reset");
 		resetButton.setToolTipText("When you want to change your mind");
-		resetButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				myItemtypeDropDown.removeActionListener(itemChangeListener);
-				myItemtypeDropDown.removeAllItems();
-				for (String item : parentList) {
-					myItemtypeDropDown.addItem(item);
-				}
-				myItemtypeDropDown = new JComboBox<String>(parentString);
-				myItemtypeDropDown.addActionListener(itemChangeListener);
-			}
-		});
+//		resetButton.addActionListener();
 		itemTypeChooserPanel.add(resetButton);
 
 		JScrollPane scrollPane = new JScrollPane();
@@ -135,5 +89,29 @@ public class ItemInputPanel extends JPanel {
 		// can show the info from the schema and allow input from the user. It should
 		// also have getters so we can query each tile for their values and upload them
 		// into items into the project with the edit controller.
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void actionPerformed(ActionEvent e) {		
+	
+//		System.out.println("?");
+		JComboBox<String> cb = (JComboBox<String>) e.getSource();
+		String parentName = (String) cb.getSelectedItem();
+		System.out.println(parentName);
+		// Get all the child type
+		List<String> childTypes = myRules.getChildTypes(parentName);
+		if (childTypes.size() != 0) {
+			// Clear out the old type
+			myItemtypeDropDown.removeActionListener(this);
+			myItemtypeDropDown.removeAllItems();
+			for (String child : childTypes) {
+				myItemtypeDropDown.addItem(child);
+			}
+			myItemtypeDropDown.addActionListener(this); 
+		} else {
+			JOptionPane.showMessageDialog(this, "There are no more child type");
+		}
+		
 	}
 }
